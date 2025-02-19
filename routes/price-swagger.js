ec00febd872b93ab5b2f5f6ca0149cc135c8fd73
Prakash-Balu <prakash.engg89@gmail.com>
2025-02-19 23:12:23 +0530

    Price api changes
    Price api changes

--- routes/price-swagger.js
+++ routes/price-swagger.js
@@ -21,3 +21,21 @@
  *       '500':
  *         description: Server Error
  */
+
+
+/**
+ * @swagger
+ * /price/getLocationPrice:
+ *  get:
+ *     tags:
+ *       - Price
+ *     responses:
+ *       '200':
+ *         description: Success
+ *       '201':
+ *         description: Created
+ *       '400':
+ *         description: Bad Request
+ *       '500':
+ *         description: Server Error
+ */
--- service/admin/price.js
+++ service/admin/price.js
@@ -141,51 +141,73 @@ module.exports = function(mongoose, utils) {
             console.log('ipAddress::', ipAddress)
             const info = await ipinfo.lookupIp(ipAddress);
             const locationInfo = await Locations.findOne({ countryCode: info.countryCode }).lean();
-
           
-            var threadObject = await LocationPlans.aggregate([
-                // { $match: { locationId: new ObjectId(locationInfo._id) } },
+            var threadObject = await Locations.aggregate([
+                { $match: { _id: locationInfo._id } },
                 {
-                    $lookup: {
-                    from: "locations", // Join with the Location collection
-                    localField: "locationId",
-                    foreignField: "_id",
-                    as: "location"
+                    $lookup:{
+                        from: "locationplans",
+                        localField: "_id",
+                        foreignField: "locationId",
+                        as: "locationDetails"
+                    }  
+                },
+                {
+                    $unwind:"$locationDetails",
+                },
+                {
+                    $unwind:"$locationDetails.availablePlans"
+                },
+                {
+                    $lookup:{
+                        from: "plans",
+                        localField: "locationDetails.availablePlans",
+                        foreignField: "_id",
+                        as: "planDetails"
                     }
                 },
                 {
-                    $unwind: "$location" // Convert array to object
+                    $unwind:"$planDetails"
                 },
                 {
-                    $lookup: {
-                    from: "plans", // Join with the Plans collection
-                    localField: "availablePlans",
-                    foreignField: "_id",
-                    as: "plans"
+                    $lookup:{
+                        from: "planprices",
+                        localField: "locationDetails._id",
+                        foreignField: "locationPlanId",
+                        as: "priceDetails"
                     }
                 },
                 {
-                    $project: {
-                    _id: 1,
-                    location: {
-                        _id: "$location._id",
-                        countryName: "$location.countryName",
-                        countryCode: "$location.countryCode",
-                        phoneCode: "$location.phoneCode",
-                        currencyCode: "$location.currencyCode",
-                        countryFlag: "$location.countryFlag",
-                        currencySymbol: "$location.currencySymbol",
-                        currencyName: "$location.currencyName"
-                    },
-                    plans: 1, // Include all plan details
-                    createdAt: 1,
-                    updatedAt: 1
+                    $group:{
+                        "_id": "$_id",
+                        "countryName": { "$first": "$countryName" },
+                        "countryCode": { "$first": "$countryCode" },
+                        "phoneCode": { "$first": "$phoneCode" },
+                        "countryFlag": { "$first": "$countryFlag" },
+                        "currencySymbol": { "$first": "$currencySymbol" },
+                        "currencyName": { "$first": "$currencyName" },
+                        "plans": { "$addToSet": "$planDetails" },
+                        "price":{"$first":"$priceDetails"},
                     }
+                },
+                {
+                    "$project": {
+                      "_id": 1,
+                      "countryName": 1,
+                      "countryCode": 1,
+                      "phoneCode": 1,
+                      "countryFlag": 1,
+                      "currencySymbol": 1,
+                      "currencyName": 1,
+                      "plans": 1,
+                      "price": 1
+                    }
+                },
+                {
+                    $sort: { "countryName": 1 } // Add this stage to sort by countryName
                 }
             ]);
 
-            console.log('====>', threadObject);
-           
             return threadObject;
         } catch (err) {
             console.log(err);
